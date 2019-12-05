d11ea3b4bce1e7202e9847070dbe595636488d6a
piotr <piotr@beesoft.pl>
2019-12-05 12:18:41 +0100

    use timeout constant

--- connector/session/session.go
+++ connector/session/session.go
@@ -2,8 +2,6 @@ package session
 
 import (
 	"Carmel/connector/stream"
-	"context"
-	"github.com/gotk3/gotk3/gtk"
 )
 
 type Session struct {
@@ -11,10 +9,10 @@ type Session struct {
 	Out *stream.Stream
 }
 
-func ServerNew(app *gtk.Application, ctx context.Context, port int) *Session {
+func ServerNew(port int) *Session {
 	return &Session{In: stream.Server(port), Out: stream.Server(port + 1)}
 }
 
-func ClientNew(app *gtk.Application, ctx context.Context, addr, name string, port, timeout int) *Session {
-	return &Session{In: stream.Client(addr, port, 120), Out: stream.Client(addr, port+1, 120)}
+func ClientNew(addr string, port, timeout int) *Session {
+	return &Session{In: stream.Client(addr, port, timeout), Out: stream.Client(addr, port+1, timeout)}
 }
